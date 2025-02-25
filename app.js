f36d28abc3b2499ae27d4ee78529e650db77804c
LuisFlores <luisflorez20@yahoo.com>
2025-02-24 19:55:55 -0500

    estoy seguro de este commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//crearemos un array para guardar los nombres de los amigos 
+// Crea un array para almacenar los nombres
 const amigos = [];
 
+//Implementa una funcion pra agregar amigos
 function agregarAmigo() {
-    //crearemos un input para que el usuario pueda ingresar el nombre del amigo
+    
     const input = document.getElementById("amigo");
     const nombre = input.value.trim();
     
@@ -13,8 +14,10 @@ function agregarAmigo() {
     }
     amigos.push(nombre);
     input.value = "";
+    actualizarLista();
 }
    
+//Implementa una funcion para actualizar la lista de amigos
 function actualizarLista() {
     const lista = document.getElementById('listaAmigos');
     lista.innerHTML = "";
@@ -22,10 +25,11 @@ function actualizarLista() {
     for(let i = 0; i < amigos.length; i++) {
         const li = document.createElement('li');
         li.textContent = amigos[i];
-        lista.appendChild(li);
+        lista.appendChild(li);      
     }
 }
 
+//Implementa una funcion para sortear los amigos
 function sortearAmigo() {
     if(amigos.length === 0 ) {
         alert('No hay amigos en la lista!');
@@ -39,4 +43,12 @@ function sortearAmigo() {
     resultado.innerHTML = `El amigo secreto es: <strong>${amigoSecreto}</strong>!`;
 }
 
-console.log(amigos);
+//boton para reiniciar el juego
+function reiniciarJuego() {
+    amigos.length = 0;
+    actualizarLista();
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+    document.getElementById('amigo').value = '';
+}
+//console.log(amigos);
